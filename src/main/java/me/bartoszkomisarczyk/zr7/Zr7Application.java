619a53a485f23ae97e20156dfe38106e8e34adcc
komisarczykb <komisarczykb@gmail.com>
2026-07-21 17:46:16 +0200

    Initializing git repository

---
+++ src/main/java/me/bartoszkomisarczyk/zr7/Zr7Application.java
@@ -0,0 +1,13 @@
+package me.bartoszkomisarczyk.zr7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zr7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zr7Application.class, args);
+	}
+
+}
